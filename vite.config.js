4a99fbef277fce30d6e8510bd75e2576c60a160b
vlad_z <zvezdinvladislav@yandex.ru>
2023-11-17 10:24:31 +0300

    wip

---
+++ vite.config.js
@@ -0,0 +1,58 @@
+import path from 'path';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import webfontDownload from 'vite-plugin-webfont-dl';
+
+export default defineConfig({
+  plugins: [
+    // laravel({
+    //   input: ['/style.css', '/main.js'],
+    //   refresh: false,
+    // }),
+    // webfontDownload([
+    //   'https://fonts.googleapis.com/css2?family=Inter:wght@700&family=Montserrat:wght@400;500;600;700&display=swap',
+    // ]),
+  ],
+  server: {
+    open: '/index.html',
+  },
+  base: './',
+  build: {
+    watch: true,
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+
+      output: {
+        entryFileNames: `assets/js/[name].js`,
+        assetFileNames: (assetInfo) => {
+          var info = assetInfo.name.split('.');
+          var extType = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          } else if (/woff|woff2|eot/.test(extType)) {
+            extType = 'fonts';
+          }
+
+          return `assets/${extType}/[name].[ext]`;
+        },
+
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+  css: {
+    devSourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'resources'),
+    },
+  },
+});
