284b30a2985555bdc049e7f50be40ea09a1424a7
vlad_z <zvezdinvladislav@yandex.ru>
2024-05-24 15:12:21 +0300

    mainpage adaptive

--- resources/js/components/mmenu.js
+++ resources/js/components/mmenu.js
@@ -5,6 +5,10 @@ export const mmenu = () => {
   function mmenuToggle() {
     burger.classList.toggle('active');
     mmenu.classList.toggle('show');
+
+    if (window.innerWidth < 768) {
+      document.body.classList.toggle('no-scroll');
+    }
   }
 
   burger.addEventListener('click', mmenuToggle);
