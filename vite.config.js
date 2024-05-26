3dca7c9ab5b60deede855b385dec0129ca04a83d
vlad_z <zvezdinvladislav@yandex.ru>
2024-05-26 15:38:25 +0300

    about_docs

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
+        about_docs: resolve(__dirname, 'about_docs.html'),
       },
 
       output: {
